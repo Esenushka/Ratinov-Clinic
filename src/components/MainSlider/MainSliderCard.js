e16fd8a3f2aa1871a9f21d24c8848b9affcf15d2
Esenushka <prosterling.mien@gmail.com>
2022-07-09 18:44:35 +0600

    feat: create main slider

---
+++ src/components/MainSlider/MainSliderCard.js
@@ -0,0 +1,25 @@
+import scss from "./MainSlider.module.scss"
+export default function MainSliderCard() {
+  return (
+    <div className={scss.card}>
+      <div className={scss.left}>
+        <h1>
+          <span>Истории и отзывы </span>
+          наших пациентов после лечения
+        </h1>
+        <p>Метод резорбции - уменьшение и заживление  грыжи,
+          вплоть до полного ее исчезновения или рубцевания.</p>
+        <span>Подробнее</span>
+        <div className={scss.btns}>
+          <button className="btn btn-big-bg">Отзывы</button>
+          <button className="btn btn-watch">Смотреть видео</button>
+        </div>
+      </div>
+      <div className={scss.right}>
+        <span className={scss.camera}>
+          <img src="/images/camera.svg" alt="Камера" />
+        </span>
+      </div>
+    </div>
+  )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import Header from "../components/Header/Header";
+import MainSlider from "../components/MainSlider/MainSlider";
 
 export default function HomePage() {
   return (
     <div>
-      <Header/>
+      <Header />
+      <MainSlider />
     </div>
   )
 }
