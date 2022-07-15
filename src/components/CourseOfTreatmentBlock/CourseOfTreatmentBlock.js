4fee0c2d7b09dcd1ddfd71544fed905daea58585
Esenushka <prosterling.mien@gmail.com>
2022-07-15 16:13:42 +0600

    feat: done course of treatment

--- src/components/CourseOfTreatmentBlock/CourseOfTreatmentBlock.js
+++ src/components/CourseOfTreatmentBlock/CourseOfTreatmentBlock.js
@@ -1,4 +1,6 @@
+import { procedureList } from "../../constants/procedureList"
 import scss from "./CourseOfTreatmentBlock.module.scss"
+import { Link } from "react-router-dom"
 
 export default function CourseOfTreatmentBlock() {
   return (
@@ -18,10 +20,10 @@ export default function CourseOfTreatmentBlock() {
         </div>
       </div>
       <div className={scss.info}>
-        <p>
+        <span>
           В норме скорость естественной резорбции невысокая - <b>около 12-15 месяцев.</b>
-          <p>Процедуры безопасны и не требуют постоянного нахождения в больнице, поэтому лечение проводится амбулаторно и занимает в день 25-50 минут.</p>
-        </p>
+          <p>Процедуры безопасны и не требуют постоянного нахождения в больнице, поэтому лечение прЗаписаться на приёмоводится амбулаторно и занимает в день 25-50 минут.</p>
+        </span>
         <p>
           Узнать о возможности лечения можно только на очной консультации у врача
         </p>
@@ -31,8 +33,17 @@ export default function CourseOfTreatmentBlock() {
           Мы создаем необходимые условия, чтобы запустить и  ускорить процесс резорбции, применяя комплексную физиотерапию:
         </h2>
         <div className={scss.procedure_blocks_wrapper}>
-          
+          {
+            procedureList.map((el) => <div className={scss.procedure_block} key={el.id}>
+              <h3>{el.title}</h3>
+              <p>{el.info}</p>
+              <span>{el.procedures}</span>
+            </div>)
+          }
         </div>
+        <Link className={scss.btn} to="/">
+          <button className="btn btn-big-bg">Записаться на приём</button>
+        </Link>
       </div>
     </div>
   )
