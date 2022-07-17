a911377a85344098c748f7d1e3bea57431e5aa29
Bezymeys <Medvedeym228>
2022-07-17 21:27:28 +0600

    feat:add block in HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,7 @@ import OwnerBlock from "../components/OwnerBlock/OwnerBlock";
 import TreatBlock from "../components/TreatBlock/TreatBlock";
 import Consultaition from "../components/Consultation/Consultation";
 import CourseOfTreatmentBlock from "../components/CourseOfTreatmentBlock/CourseOfTreatmentBlock";
+import DoctorSlider from "../components/DoctorSlider/DoctorSlider";
 
 export default function HomePage() {
   return (
@@ -16,6 +17,7 @@ export default function HomePage() {
       <OwnerBlock />
       <YouTubeSlider />
       <CourseOfTreatmentBlock />
+      <DoctorSlider />
     </div>
   )
 }
