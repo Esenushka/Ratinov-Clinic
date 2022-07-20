bc5dd6d20ac8bc85c57a90f11b18d3eda7188cfe
Esenushka <prosterling.mien@gmail.com>
2022-07-20 12:50:56 +0600

    feat: add maping

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,16 +1,24 @@
+import { FAQList } from "../../constants/FAQList"
 import scss from "./FAQ.module.scss"
 import FAQCard from "./FAQCard"
 
 export default function FAQ() {
-
+  const part1 = FAQList.slice(0, FAQList.length / 2)
+  const part2 = FAQList.slice(FAQList.length / 2)
   return (
     <div className={scss.wrapper + " container"}>
       <h1>ВОПРОСЫ</h1>
       <div className={scss.blocks_wrapper}>
-        <FAQCard />
-        <FAQCard />
-        <FAQCard />
-        <FAQCard />
+        <div>
+          {
+            part1.map((el) => <FAQCard key={el.id} {...el} />)
+          }
+        </div>
+        <div>
+          {
+            part2.map((el) => <FAQCard key={el.id} {...el} />)
+          }
+        </div>
       </div>
     </div>
   )
--- src/components/FAQ/FAQCard.js
+++ src/components/FAQ/FAQCard.js
@@ -1,24 +1,71 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 import scss from "./FAQ.module.scss"
 
-export default function FAQCard() {
+export default function FAQCard({ title, buttons, info, type }) {
   const [active, setActive] = useState(false);
 
   return (
     <div className={scss.block}>
       <span onClick={() => setActive(!active)}>
-        <img src="/images/slider-arrow.svg" alt="Стрелка" />
-        <p>Какие заболевания вы лечите?</p>
+        <img className={active ? scss.active : ""} src="/images/slider-arrow.svg" alt="Стрелка" />
+        <p>{title}</p>
       </span>
       <div className={scss.info_wrapper + " " + (active ? scss.active : "")}>
-        <div className={scss.info}>
-          Запишитесь на очный прием к нашему неврологу-вертебрологу,  чтобы получить направление на МРТ со скидкой -500 сомов в ДЦ «Бонецкий»
-        </div>
-        <div className={scss.btns}>
-          <button className="btn btn-small">Невролог - вертебролог </button>
-          <button className="btn btn-small">Ортопед-травматолог </button>
-          <button className="btn btn-small">Массажист </button>
-        </div>
+        {
+          type === 1 ? <div className={scss.info + " " + scss.bold}>
+            <p>{info[0]}</p>
+            <p>{info[1]}</p>
+          </div> : type === 1.1 ?
+            <div className={scss.info + " " + scss.bold_reverse}>
+              <p>{info[0]}</p>
+              <p>{info[1]}</p>
+            </div> : type === 2 ?
+              <div className={scss.info}>
+                <p><span>{info[0]}</span> - {info[1]}</p>
+                <p>{info[2]}</p>
+              </div> : type === 3 ?
+                <div className={scss.info + " " + scss.with_link}>
+                  {
+                    info.map((el) => el.reverse ?
+                      <p key={el.id} >
+                        {el.text}
+                        {" "}
+                        <Link to={el.path}>{el.link}</Link>
+                      </p>
+                      : <p key={el.id} >
+                        <Link to={el.path}>{el.link}</Link>
+                        {" "}
+                        {el.text}
+                      </p>)
+                  }
+                </div> : type === "phone" ?
+                  <div className={scss.info + " " + scss.phone}>
+                    {
+                      info.map((el) => <p key={el.id}>
+                        <img src="/images/HeaderPhone.svg" />
+                        {el.phone}
+                        {el.whatsapp ? " (WhatsApp)" : ""}
+                      </p>)
+                    }
+                  </div> : type === 4 ?
+                    <div className={scss.info}>
+                      {info.map((el, index) => <p key={index}>{el}</p>)}
+                    </div> : type === 5 ?
+                      <div className={scss.info}>
+                        {
+                          info.map((el) => <p key={el.id}>{el.text}<span> {el.bold}</span></p>)
+                        }
+                      </div> : ""
+        }
+
+        {
+          buttons.length > 0 ? <div className={scss.btns + " " + (buttons?.length === 1 ? scss.only_one : "")}>
+            {
+              buttons.map((el, index) => <button key={index} className="btn btn-small">{el}</button>)
+            }
+          </div> : ""
+        }
       </div>
     </div>
   )
