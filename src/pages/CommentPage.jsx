1d800b7cdc8d19454e211b0546fded0a47d3770e
Bezymeys <Medvedeym228>
2022-07-26 13:54:21 +0600

    feat:comment page

---
+++ src/pages/CommentPage.jsx
@@ -0,0 +1,18 @@
+import CallMe from "../components/CallMe/CallMe";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import YouTubeSlider from "../components/YouTubeSlider/YouTubeSlider";
+
+export default function CommentPage() {
+  return (
+    <div>
+      <Header />
+      <div>
+        
+      </div>
+      <YouTubeSlider />
+      <CallMe />
+      <Footer />
+    </div>
+  );
+}
