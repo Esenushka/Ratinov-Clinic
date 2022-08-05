57bbaddfbd56812d11fed7f27b75d9a90836aded
Esenushka <prosterling.mien@gmail.com>
2022-08-06 01:00:38 +0600

    feat: done botulinum-page

--- src/pages/services/BotulinumPage.js
+++ src/pages/services/BotulinumPage.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Botulinum from '../../components/Botulinum/Botulinum'
+import CallMe from '../../components/CallMe/CallMe'
+import Footer from '../../components/Footer/Footer'
 import Header from '../../components/Header/Header'
 import TopBlock from '../../components/TopBlock/TopBlock'
 
@@ -7,7 +10,9 @@ export default function BotulinumPage() {
     <div>
       <Header />
       <TopBlock bold={"Ботулинотерапия"} path={"Услуги"} secondPath={"Ботулинотерапия"} />
-      
+      <Botulinum />
+      <CallMe />
+      <Footer />
     </div>
   )
 }
