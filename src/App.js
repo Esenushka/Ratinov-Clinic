59673e01f64af3976b38c39b2b7a4ae50ed71e1c
Esenushka <prosterling.mien@gmail.com>
2022-08-05 22:09:17 +0600

    feat: create botulinum-page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Price from './pages/Price';
 import MassagePage from './pages/services/MassagePage';
 import PhysiotherapyPage from './pages/services/PhysiotherapyPage';
 import StocksPage from './pages/StocksPage';
+import BotulinumPage from './pages/services/BotulinumPage';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/services/massage" element={<MassagePage />}></Route>
         <Route path="/services/physiotherapy" element={<PhysiotherapyPage />}></Route>
         <Route path="/services/stocks" element={<StocksPage />}></Route>
+        <Route path="/services/botulinum" element={<BotulinumPage />}></Route>
       </Routes>
     </>
   );
---
+++ src/pages/services/BotulinumPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import TopBlock from '../../components/TopBlock/TopBlock'
+
+export default function BotulinumPage() {
+  return (
+    <div>
+      <Header />
+      <TopBlock bold={"Ботулинотерапия"} path={"Услуги"} secondPath={"Ботулинотерапия"} />
+      
+    </div>
+  )
+}
