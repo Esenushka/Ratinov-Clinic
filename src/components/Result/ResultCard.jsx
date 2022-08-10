05519559cf9258ca58ef5577771bbdee444f7afe
Bezymeys <Medvedeym228>
2022-08-10 17:11:04 +0600

    feat:add result card & modal window

---
+++ src/components/Result/ResultCard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import css from "./Result.module.scss"
+
+export default function ResultCard({ img, num, afterComplaints, afterDisease, appointment, beforeComplaints, beforeDisease, time, timeDes}) {
+  const [modalActive, setModalActive] = useState(false)
+  return (
+    <>
+      <div className={modalActive ? css.active : css.modal} onClick={(e) => {
+        e.stopPropagation()
+        setModalActive(false)
+      }}>
+        <div className={`${css.modal__content} container`} onClick={(e) => {
+          e.stopPropagation()
+        }}>
+          <div className={css.card}>
+            <p>Снимок № {num}</p>
+            <img src={img} alt="result_photo" />
+          </div>
+          <div className={css.modalRight__block}>
+            <div className={css.before}>
+              <h4>+ Жалобы до лечения: </h4>
+              <div className={css.des}>
+                <p>{beforeComplaints}</p>
+                <span>{beforeDisease}</span>
+              </div>
+            </div>
+            <h4>Назначили: {appointment}</h4>
+            <div className={css.after}>
+              <h4>- После лечения: </h4>
+              <div className={css.des}>
+                <span>{afterDisease}</span>
+                <p>{afterComplaints}</p>
+              </div>
+            </div>
+            <div>
+              <p>{time}</p>
+              <p>{timeDes}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={css.card} onClick={() => setModalActive(true)}>
+        <img src={img} alt="result_photo" />
+        <p>Снимок № {num}</p>
+      </div>
+    </>
+  )
+}
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,12 +1,36 @@
+import css from "../components/Result/Result.module.scss"
 import CallMe from "../components/CallMe/CallMe"
+import Footer from "../components/Footer/Footer"
 import Header from "../components/Header/Header"
 import TopBlock from "../components/TopBlock/TopBlock"
+import { db } from "../config/firebase"
+import { useEffect, useState } from "react"
+import ResultCard from "../components/Result/ResultCard"
 
 export default function ResultPage() {
+  const [result, setResult] = useState([])
+  useEffect(() => {
+    db.collection("result")
+      .get()
+      .then((snapshot) => {
+        const resultArr = []
+        snapshot.forEach((e) => {
+          resultArr.push({ ...e.data(), id: e.id });
+        })
+        setResult(resultArr.sort((a, b) => parseFloat(a.pos) - parseFloat(b.pos)))
+      })
+  }, []);
   return (
     <div>
       <Header />
-      <TopBlock />
+      <TopBlock path={'Результаты'} bold={'Результаты'} />
+      <div className={`container ${css.result__wrapper}`} >
+        {
+          result.map((e) => (
+            <ResultCard  key={e.id} {...e} />
+          ))
+        }
+      </div>
       <CallMe />
       <Footer />
     </div>
