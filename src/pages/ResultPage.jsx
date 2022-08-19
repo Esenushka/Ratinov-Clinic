5455cbb65e21c94f844c2fc10b294584cad42063
Esenushka <prosterling.mien@gmail.com>
2022-08-19 20:47:05 +0600

    fix: filter

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -6,18 +6,18 @@ import TopBlock from "../components/TopBlock/TopBlock"
 import { db } from "../config/firebase"
 import { useEffect, useState } from "react"
 import ResultCard from "../components/Result/ResultCard"
+import { useLocation, useNavigate } from "react-router-dom"
 
 export default function ResultPage() {
   const [result, setResult] = useState([])
-  const [filtered, setFiltered] = useState([result])
-
-  function resultFilter(type) {
-    if (type === 'all') {
-      setFiltered(result)
-    } else {
-      const newResult = [...result].filter(e => e.type === type)
-      setFiltered(newResult)
-    }
+  const location = useLocation();
+  const navigate = useNavigate();
+  const paramEntries = new URLSearchParams(location.search).entries()
+  const fromEntries = Object.fromEntries(paramEntries)
+  const paramArr = Object.keys(fromEntries);
+  const handleChange = (type) => {
+    type === "all" ? navigate("/result") :
+      navigate(location.pathname + "?" + type)
   }
 
   useEffect(() => {
@@ -28,7 +28,7 @@ export default function ResultPage() {
         snapshot.forEach((e) => {
           resultArr.push({ ...e.data(), id: e.id });
         })
-        setResult(resultArr.sort((a, b) => parseFloat(a.pos) - parseFloat(b.pos)))
+        setResult(resultArr.sort((a, b) => parseFloat(a.pos) - parseFloat(b.pos)));
       })
   }, []);
   return (
@@ -41,17 +41,32 @@ export default function ResultPage() {
           <span>Вдохновляют и дарят надежду!</span>
         </div>
         <div className={scss.btn__group}>
-          <button className="btn btn-small" onClick={() => resultFilter('all')}>Все</button>
-          <button className="btn btn-small" onClick={() => resultFilter('neck')}>Шейный отдел</button>
-          <button className="btn btn-small" onClick={() => resultFilter('lumbar')}>Поясничный отдел</button>
+          <button
+            className={"btn btn-small " + (paramArr.length === 0 ? scss.btnActive : "")}
+            onClick={() => handleChange('all')}>
+            Все
+          </button>
+          <button
+            className={"btn btn-small " + (paramArr.some((param) => param === "neck") ? scss.btnActive : "")}
+            onClick={() => handleChange('neck')}>
+            Шейный отдел
+          </button>
+          <button
+            className={"btn btn-small " + (paramArr.some((param) => param === "lumbar") ? scss.btnActive : "")}
+            onClick={() => handleChange('lumbar')}>
+            Поясничный отдел
+          </button>
         </div>
         <div className={scss.result}>
           {
-            filtered.map((e) => (
-              <ResultCard key={e.id} {...e} />
+            result.map((e) => (
+              paramArr.length === 0 ?
+                <ResultCard key={e.id} {...e} /> :
+                paramArr.map((param) =>
+                  e.type === param ?
+                    <ResultCard key={e.id} {...e} /> : "")
             ))
           }
-
         </div>
       </div>
       <CallMe />
