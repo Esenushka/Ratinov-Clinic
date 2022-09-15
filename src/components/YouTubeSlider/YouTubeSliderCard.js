7b8bfb251088207d21e631c2022a0caa8053d34b
Adilxxan <adilhanimerov82@gmail.com>
2022-09-15 13:48:28 +0600

    feat/video in youtube

--- src/components/YouTubeSlider/YouTubeSliderCard.js
+++ src/components/YouTubeSlider/YouTubeSliderCard.js
@@ -2,10 +2,24 @@ import scss from "./YouTubeSlider.module.scss"
 import { memo } from "react"
 
 export default memo(function YouTubeSliderCard({ ...el }) {
+  console.log(el.img);
   return (
     <div className={scss.card}>
       <span className={scss.camera}>
-        <img src="/images/camera.svg" alt={el.name} />
+        {
+          el.img
+            ?
+            <iframe
+              width="290"
+              height="190"
+              src={el.img}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen>
+            </iframe>
+            : <img src="/images/camera.svg" alt={el.name} />
+        }
       </span>
       <a href={el.video}>
         {el.name}
