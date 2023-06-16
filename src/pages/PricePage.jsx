5b8b832661f9a9c41b9dffadbc0a95e1e8fb962f
aiskairr <aminhansss2222@gmail.com>
2023-06-16 21:34:53 +0600

    feat/fix width and img

--- src/pages/PricePage.jsx
+++ src/pages/PricePage.jsx
@@ -33,7 +33,7 @@ export default React.memo(function Price() {
       {isHeader && (
         <>
           <TopBlock bold={"Цены"} path={"Цены"} />
-          <img width={"100%"} src="./images/prices.jpg" alt="prices" />
+          <img className="imgWidth" src="./images/prices.jpg" alt="prices" />
           {price.map((e) => (
             <PriceCard key={e.id} {...e}  isHeader={isHeader}/>
           ))}
